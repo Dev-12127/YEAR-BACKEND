fc19f854428e4f323e1f4d4547cd9fd76cfc88ab
Toure Lassana <l.toure910@laposte.net>
2024-09-20 15:07:40 +0200

    test

--- routes/year.js
+++ routes/year.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  const current = new Date().getFullYear();
+  const current = new Date().getFullYear().toString();
   res.json({ now: current });
  });
 
